a517df8253cb27da95118fa80a8231a7c1daabed
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2010-03-06 20:06:16 +0000

    Beginnings of the document repository module.

--- lib/document.rb
+++ lib/document.rb
@@ -1,10 +1,12 @@
 require 'rubygems'
 require 'mongo'
 
-require 'document/serialization'
+require "document/serialization"
+require "document/repository"
 
 module LightMongo
   module Document
     include Serialization
+    include Repository
   end
 end
--- spec/document_spec.rb
+++ spec/document_spec.rb
@@ -1,11 +1,15 @@
 require File.dirname(__FILE__) + '/../lib/document'
 
 describe LightMongo::Document do
+  class TestClass
+    include LightMongo::Document
+  end
+
   it "loads the serialization module" do
-    class TestClass
-      include LightMongo::Document
-    end
-    
     TestClass.should include(LightMongo::Document::Serialization)
+  end
+  
+  it "loads the repository module" do
+    TestClass.should include(LightMongo::Document::Repository)
   end
 end
