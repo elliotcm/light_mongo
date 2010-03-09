1a29fa6943ae5dbdd55fcaf5ef81d22c62f1847f
Steve Tooke <steve.tooke@gmail.com>
2010-03-09 23:04:03 +0000

    Fix stupidly adding methods to Class

--- lib/document/persistence.rb
+++ lib/document/persistence.rb
@@ -65,7 +65,7 @@ module LightMongo
           
           method_name = 'find_by_'+(options[:as] or key_name).to_s
           if viable_method_name(method_name)
-            self.class.class_eval do
+            (class << self; self; end).class_eval do
               define_method method_name.to_sym do |value|
                 collection.find(key_name.to_sym => value).map{|bson_hash| new(bson_hash)}
               end
