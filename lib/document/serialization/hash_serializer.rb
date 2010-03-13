f4d1dae45c8c78a751c9544f0d94508cee8e0ccb
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2010-03-13 12:49:59 +0000

    HashSerializer handles Arrays.

--- lib/document/serialization/hash_serializer.rb
+++ lib/document/serialization/hash_serializer.rb
@@ -2,6 +2,11 @@ module LightMongo
   module Document
     module Serialization
       class HashSerializer
+        def self.dump(object_to_serialize, current_depth=0)
+          object_to_serialize.map do |entry|
+            Serializer.serialize(entry, current_depth + 1)
+          end
+        end
       end
     end
   end
--- spec/document/serialization/hash_serializer_spec.rb
+++ spec/document/serialization/hash_serializer_spec.rb
@@ -1,29 +1,62 @@
-require File.dirname(__FILE__) + '/../../../lib/document/serialization/hash_serializer'
+require File.dirname(__FILE__) + '/../../../lib/document/serialization'
 
-HashSerializer = LightMongo::Document::Serialization::HashSerializer
+Serialization = LightMongo::Document::Serialization
+HashSerializer = Serialization::HashSerializer
 
 describe HashSerializer do
-  it "deep runs a hash" do
+  describe ".dump(object, current_depth)" do
+    context "when the object is an Array" do
+      before(:each) do
+        @current_depth = 0
+        
+        @object = [
+          @sub_object_1 = mock(:sub_object_1),
+          @sub_object_2 = mock(:sub_object_2)
+        ]
+        
+        Serialization::Serializer.
+          stub!(:serialize).
+          with(@sub_object_1, anything).
+          and_return(@serial_1 = mock(:serial_1))
+        Serialization::Serializer.
+          stub!(:serialize).
+          with(@sub_object_2, anything).
+          and_return(@serial_2 = mock(:serial_2))
+      end
+      
+      it "iterates over the array, serializing the contents." do
+        HashSerializer.dump(@object).should == [@serial_1, @serial_2]
+      end
+      
+      it "notifies the generic Serializer of the new depth." do
+        Serialization::Serializer.should_receive(:serialize).with(@sub_object_1, @current_depth + 1)
+        HashSerializer.dump(@object, @current_depth)
+      end
+      
+      it "returns an array." do
+        HashSerializer.dump(@object).should be_an(Array)
+      end
+    end
+    
+  end
+  
+  xit "deep runs a hash" do
     pending
   end
   
-  it "deep runs an array" do
+  xit "deep runs any custom object" do
     pending
   end
   
-  it "deep runs any custom object" do
+  xit "employs a Serializer on any attributes which are containers of some sort" do
     pending
   end
   
-  it "employs a Serializer on any attributes which are containers of some sort" do
+  xit "does a simple copy of any non-container attributes into the outbound container" do
     pending
   end
   
-  it "does a simple copy of any non-container attributes into the outbound hash" do
-    pending
-  end
-  
-  it "does a post-serialization copy of any container attributes into the outbound hash" do
+  xit "does a post-serialization copy of any container attributes into the outbound container" do
     pending
   end
 end
