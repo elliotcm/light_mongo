be8ec75d0356f2be17c0c47565472df3278fd1d2
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2010-03-27 14:11:44 +0000

    Rewrite create/read integration test.
    This is in preparation for full CRUD support.

--- integration/basic_storage_cycle_spec.rb
+++ integration/basic_storage_cycle_spec.rb
@@ -6,12 +6,14 @@ end
 
 describe 'The basic storage cycle' do
   before(:each) do
-    @geology_article = Article.new(:title => (@title = 'Fluid Physics in Geology'),
-                                   :abstract => (@abstract = 'Lorem ipsum dolor..'))
+    @geology_params = {
+      :title => (@title = 'Fluid Physics in Geology'),
+      :abstract => (@abstract = 'Lorem ipsum dolor..')
+    }
   end
   
-  it "allows the storage and retrieval of documents." do
-    @geology_article.save
+  it "creates and reads." do
+    Article.create(@geology_params)
     stored_article = Article.find.first
     stored_article.instance_variable_get('@title').should == @title
     stored_article.instance_variable_get('@abstract').should == @abstract
